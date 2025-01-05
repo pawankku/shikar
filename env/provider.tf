0bfcf993bd390b4a659bf3a6886e895ee79745a8
pawankku <kkpawangupta@gmail.com>
2025-01-05 18:25:33 +0530

    backend

--- env/provider.tf
+++ env/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.14.0"
     }
   }
+    backend "azurerm" {
+    resource_group_name  = "pawan-rg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "storage53637337"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "container"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    use_azuread_auth     = true                            # Can also be set via `ARM_USE_AZUREAD` environment variable.
+  }
 }
 
 provider "azurerm" {
